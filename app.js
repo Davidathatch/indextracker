df1da79e12277fd86e904a43bcd0462e17558204
David Thatcher <54860843+Davidathatch@users.noreply.github.com>
2020-06-28 20:13:32 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ anychart.onDocumentReady(function () {
   let name10 = document.querySelectorAll(".name")[9].innerHTML;
   let name11 = document.querySelectorAll(".name")[10].innerHTML;
 
+  var participants = [
+    name1,
+    name2,
+    name3,
+    name4,
+    name5,
+    name6,
+    name7,
+    name8,
+    name9,
+    name10,
+    name11,
+  ];
+
   let score1 = document.querySelectorAll(".score")[0].innerHTML;
   let score2 = document.querySelectorAll(".score")[1].innerHTML;
   let score3 = document.querySelectorAll(".score")[2].innerHTML;
@@ -38,6 +52,14 @@ anychart.onDocumentReady(function () {
     score10,
     score11,
   ];
+
+  let firstPlaceSpot = document.querySelector(".first-place h2");
+  let secondPlaceSpot = document.querySelector(".second-place h2");
+  let thirdPlaceSpot = document.querySelector(".third-place h2");
+
+  let firstPlaceScore = document.querySelector(".first-place h4");
+  let secondPlaceScore = document.querySelector(".second-place h4");
+  let thirdPlaceScore = document.querySelector(".third-place h4");
 
   var data = [
     { x: name1, value: score1 },
@@ -73,8 +95,9 @@ anychart.onDocumentReady(function () {
 
   credits.style.display = "none";
 
+  //GETS FIRST PLACE
+
   let winner = [0];
-  console.log(scores[winner[0]]);
 
   for (let i = 1; i <= 11; i++) {
     if (scores[i] > scores[winner[0]]) {
@@ -88,10 +111,6 @@ anychart.onDocumentReady(function () {
         winner[winner.length] = i;
       }
     }
-  }
-
-  for (let i = 0; i < winner.length; i++) {
-    console.log(winner[i]);
   }
 
   for (let x = 0; x < winner.length; x++) {
@@ -114,5 +133,49 @@ anychart.onDocumentReady(function () {
   } else {
     winnerbanner.innerHTML =
       winner.length + " People Have Indexed The Most Names";
+  }
+
+  //GETS TOP THREE
+
+  let leaderboard = [
+    score1,
+    score2,
+    score3,
+    score4,
+    score5,
+    score6,
+    score7,
+    score8,
+    score9,
+    score10,
+    score11,
+  ];
+
+  for (let i = 0; i < leaderboard.length; i++) {
+    for (let x = 0; x < leaderboard.length; x++) {
+      let current = leaderboard[i];
+      let currentParticipant = participants[i];
+      if (leaderboard[i] < leaderboard[x]) {
+        leaderboard[i] = leaderboard[x];
+        leaderboard[x] = current;
+        participants[i] = participants[x];
+        participants[x] = currentParticipant;
+      }
+    }
+  }
+
+  firstPlaceSpot.innerHTML = participants[participants.length - 1];
+  secondPlaceSpot.innerHTML = participants[participants.length - 2];
+  thirdPlaceSpot.innerHTML = participants[participants.length - 3];
+
+  firstPlaceScore.innerHTML =
+    leaderboard[leaderboard.length - 1] + " Names Indexed";
+  secondPlaceScore.innerHTML =
+    leaderboard[leaderboard.length - 2] + " Names Indexed";
+  thirdPlaceScore.innerHTML =
+    leaderboard[leaderboard.length - 3] + " Names Indexed";
+
+  for (let x = 0; x < leaderboard.length; x++) {
+    console.log(leaderboard[x]);
   }
 });
